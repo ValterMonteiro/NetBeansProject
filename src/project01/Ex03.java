d7adf1b75f9bf6b971691e88ad084003d43b294d
ValterMonteiro <valtinms@gmail.com>
2022-07-28 22:17:06 -0400

    new commit

--- src/project01/Ex03.java
+++ src/project01/Ex03.java
@@ -14,6 +14,7 @@ public class Ex03 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
+            System.out.println("Tabuado do: " + i);
             for (int j = 1; j <= 10; j++){
             int r = i * j;
             System.out.printf("%d x 2 = %d\n", i, r);
