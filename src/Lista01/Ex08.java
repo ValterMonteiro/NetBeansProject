ff6967fd27bca164037177436dad08d03b45ed1b
ValterMonteiro <valtinms@gmail.com>
2022-07-31 17:28:02 -0400

    new commit

--- src/Lista01/Ex08.java
+++ src/Lista01/Ex08.java
@@ -4,14 +4,58 @@
  */
 package Lista01;
 
+import java.util.Scanner;
+
 /**
  *
- * @author usuario
- * 8. Faça o programa que leia o nome, o peso, a idade e altura de 20 pessoas e ao final
-informe: O nome da pessoa mais velha, o nome da pessoa mais nova, a média de
-peso das pessoas com mais de 25 anos e altura média das pessoas com menos de
-10 anos.
+ * @author usuario 
+ * 8. Faça o programa que leia o nome, o peso, a idade e altura
+ * de 20 pessoas e ao final informe: 
+ * O nome da pessoa mais velha, 
+ * o nome da pessoa mais nova, 
+ * a média de peso das pessoas com mais de 25 anos e 
+ * altura média das pessoas com menos de 10 anos.
  */
 public class Ex08 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        int idade, medPesoAcima25, medAlturaMenos10, maisVelha = 0, maisNova = 0;
+        float peso = 0, somaPeso = 0, somaAltura = 0, altura = 0, pesoMaior25 = 0, alturaMenor10 = 0;
+
+        for (int i = 0; i < 2; i++) {
+
+            System.out.println("Informe o nome: ");
+            nome = sc.next();
+
+            System.out.println("Informe o peso: ");
+            peso = sc.nextFloat();
+
+            System.out.println("Informe a idade: ");
+            idade = sc.nextInt();
+            if (idade > maisVelha) {
+                maisVelha = idade;
+            }
+            if (idade < maisNova) {
+                maisNova = idade;
+            }
+            if (idade > 25) {
+                pesoMaior25++;
+                somaPeso = somaPeso + pesoMaior25;
+            }
+
+            System.out.println("Informe a altuar: ");
+            altura = sc.nextFloat();
+            if (idade < 10) {
+                alturaMenor10++;
+                somaAltura = somaAltura + alturaMenor10;
+            }
+        }
+            System.out.println("A pessoa mais velha é: " + nome);
+            System.out.println("A pessoa mais nova é: " + nome);
+            medPesoAcima25 = (int) (somaPeso / 2);
+            medAlturaMenos10 = (int) ( somaAltura/ 2);
+    }
 }
