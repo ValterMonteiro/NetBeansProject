3a37887a3e97e56a926f2c7d40ef4a9d7a6e790d
ValterMonteiro <valtinms@gmail.com>
2022-07-31 17:27:56 -0400

    new commit

--- src/Lista01/Ex07.java
+++ src/Lista01/Ex07.java
@@ -4,13 +4,37 @@
  */
 package Lista01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author usuario
  * 7. Faça um programa que receba a idade e o peso de sete pessoas. Calcule e mostre:
-A quantidade de pessoas com mais de 90 quilos; A média das idades das sete
+   A quantidade de pessoas com mais de 90 quilos; 
+*  A média das idades das sete
 pessoas;
  */
 public class Ex07 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int idade, medIdade, maior90k = 0;
+        float peso = 0, soma = 0;
+        
+        for( int i = 0; i < 7; i ++){
+            System.out.println("Informe a idade: ");
+            idade = sc.nextInt();
+            
+            System.out.println("Informe o peso: ");
+            peso = sc.nextFloat();
+            
+            if(peso > 90){
+                maior90k ++;
+            }
+            soma = soma + idade;
+        }
+        medIdade = (int) (soma/7);
+        System.out.println("Total de pessoas com mais de 90k: " + maior90k);
+        System.out.println("A média de idade é: " + medIdade);
+    }
 }
