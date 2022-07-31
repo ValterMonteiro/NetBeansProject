1cf8a2171d8283c0494fdba314a5c6323df4dec7
ValterMonteiro <valtinms@gmail.com>
2022-07-30 22:26:21 -0400

    new commit

--- src/Lista01/Ex01.java
+++ src/Lista01/Ex01.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 
 /**
  *
- * @author usuario 1. Faça um um programa em Java que leia a idade de uma pessoa
+ * @author usuario 
+ * 1. Faça um um programa em Java que leia a idade de uma pessoa
  * expressa em anos, meses e dias e mostre-a expressa em dias. Leve em
  * consideração o ano com 365 dias e o mês com 30. (Ex: 3 anos, 2 meses e 15
  * dias = 1170 dias.)
