f328c70773c7af1c371d320b10e309facb94dbdd
ValterMonteiro <valtinms@gmail.com>
2022-08-01 21:28:59 -0400

    new commit

--- src/Lista01/Ex11.java
+++ src/Lista01/Ex11.java
@@ -4,13 +4,34 @@
  */
 package Lista01;
 
+import java.util.Scanner;
+
 /**
  *
- * @author usuario
- * 11. A série de Fibonacci é formada pela sequência 1,1,2,3,5,8,13,21,34,55,... Faça um
-programa capaz de gerar a série até o n−ésimo termo que deve ser informado pelo
-usuário.
+ * @author usuario 11. A série de Fibonacci é formada pela sequência
+ * 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o
+ * n−ésimo termo que deve ser informado pelo usuário.
  */
 public class Ex11 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = 1, num2 = 0, valor;
+
+        System.out.println("Informe o valor: ");
+        valor = sc.nextInt();
+
+        for (int i = 0; i < valor; i++) {
+
+            System.out.println(num2);
+            System.out.println(num1);
+            
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+
+            System.out.println(num1);
+
+        }
+    }
 }
