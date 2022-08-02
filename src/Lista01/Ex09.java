2c48003dfd60134557e7ab34add2cd0a881492e5
ValterMonteiro <valtinms@gmail.com>
2022-08-01 20:44:17 -0400

    new commit

--- src/Lista01/Ex09.java
+++ src/Lista01/Ex09.java
@@ -4,12 +4,39 @@
  */
 package Lista01;
 
+import java.util.Scanner;
+
 /**
  *
- * @author usuario
- * 9. Faça um programa que receba dois números inteiros e gere os números inteiros
-que estão no intervalo compreendido por eles.
+ * @author usuario 9. Faça um programa que receba dois números inteiros e gere
+ * os números inteiros que estão no intervalo compreendido por eles.
  */
 public class Ex09 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int primeiroNumero, segundoNumero;
+
+        System.out.println("Informe o primeiro número: ");
+        primeiroNumero = sc.nextInt();
+
+        System.out.println("Informe o segunfo número: ");
+        segundoNumero = sc.nextInt();
+        
+        System.out.println("Veja o intervalo entre os números:");
+
+        if (primeiroNumero < segundoNumero) {
+            for (int i = primeiroNumero; i <= segundoNumero; i++) {
+                System.out.println(i);
+
+            }
+        } else {
+            for (int i = segundoNumero; i <= primeiroNumero; i++) {
+                System.out.println(i);
+            }
+
+        }
+    }
 }
