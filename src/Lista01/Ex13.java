aba0ad8e939220539310e339edb6a3c7f7d97372
ValterMonteiro <valtinms@gmail.com>
2022-08-04 21:56:57 -0400

    new commit

--- src/Lista01/Ex13.java
+++ src/Lista01/Ex13.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Lista01;
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +13,29 @@ programa que leia um conjunto indeterminado de temperaturas, e informe ao final 
 menor e a maior temperaturas informadas, bem como a média das temperaturas.
  */
 public class Ex13 {
-    
+    public static void main(String[] args) {
+        float temp, maior=0, menor=0, soma=0, media;
+        int quant=0, continua=1;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Digite uma temperatura: ");
+            temp = sc.nextFloat();
+            quant++;
+            soma += temp;
+            if (quant == 1 || temp > maior) {
+                maior = temp;
+            }
+            if (quant == 1 || temp < menor) {
+                menor = temp;
+            }
+            do {
+                System.out.println("1: continua e 0: encerra");
+                continua =  sc.nextInt();
+            } while (continua != 1 && continua != 0);
+        } while (continua == 1);
+        System.out.println("Maior Temperatura: " + maior);
+        System.out.println("Menor Temperatura: " + menor);
+        media = soma / quant;
+        System.out.println("Média das temperaturas: " + media);
+    }
 }
