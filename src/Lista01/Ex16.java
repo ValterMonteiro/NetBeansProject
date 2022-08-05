61811ed35826ea59f118e664cf74cab246bd412d
ValterMonteiro <valtinms@gmail.com>
2022-08-04 22:21:24 -0400

    new commit

--- src/Lista01/Ex16.java
+++ src/Lista01/Ex16.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Lista01;
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +13,24 @@ será digitado pelo usuário, mas a tabuada não deve necessariamente iniciar em
 terminar em 10, o valor inicial e final devem ser informados também pelo usuário.
  */
 public class Ex16 {
-    
+    public static void main(String[] args) {
+    int num, inicio, fim, aux;
+    boolean primo;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digite um número inteiro: ");
+    num = sc.nextInt();
+    System.out.println("digite o valor de inicio: ");
+    inicio = sc.nextInt();
+    System.out.println("digite o valor de fim: ");
+    fim = sc.nextInt();
+    if (inicio > fim) {
+      aux = inicio;
+      inicio = fim;
+      fim = aux;
+    }
+    for (int i=inicio; i <= fim; i++){
+      System.out.println(i + " x " + num + " = " + (i*num));
+    }
+  }
 }
+
