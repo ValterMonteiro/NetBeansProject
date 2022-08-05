8522c023355bd5183fbab6d488e43f03d8cbf711
ValterMonteiro <valtinms@gmail.com>
2022-08-04 22:11:57 -0400

    new commit

--- src/Lista01/Ex14.java
+++ src/Lista01/Ex14.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Lista01;
+import java.util.Scanner;
 
 /**
  *
@@ -14,5 +15,22 @@ um número primo.
 
  */
 public class Ex14 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        int num = sc.nextInt();
+        boolean primo;
+        primo = true;
+        for(int i= 2; i <= num/2; i++ ){
+            if(num % i == 0){
+                primo = false;
+                break;
+            }
+        }
+        if(primo){
+            System.out.println("Número é primo: ");
+        }else{
+            System.out.println("Número não é primo: ");
+        }
+    }
 }
