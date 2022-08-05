70d9cb6f407bf4933336e666528e3094ec637571
ValterMonteiro <valtinms@gmail.com>
2022-08-04 22:20:25 -0400

    new commit

--- src/Lista01/Ex15.java
+++ src/Lista01/Ex15.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Lista01;
+import java.util.Scanner;
 
 /**
  *
@@ -11,5 +12,23 @@ package Lista01;
 dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
  */
 public class Ex15 {
-    
+     public static void main(String[] args) {
+    int num;
+    boolean primo;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digite um número inteiro: ");
+    num = sc.nextInt();
+    for (int j=1; j <= num; j++){
+      primo = true;
+      for (int i=2; i <= j/2; i++){
+        if (j % i == 0){
+          primo = false;
+          break;
+        }
+      }
+      if (primo) { 
+        System.out.println(j);
+      } 
+    }
+  }
 }
