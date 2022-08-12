58e48910da78d7ade13796718dbda195b01878ec
ValterMonteiro <valtinms@gmail.com>
2022-08-11 20:43:30 -0400

    new commit

---
+++ src/Ex01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ * 
+ * Crie uma classe que simule a jogada de um dado de seis lados
+(números de 1 a 6) por três vezes. Ao final some seus valores e
+apresente o resultado das três jogadas. Veja o resultado dos lançamentos
+na Figura 4.15.
+ */
+public class Ex01 {
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ src/Ex02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ * Uma farmácia precisa ajustar os preços de seus produtos em
+12%. Elabore uma classe que receba o valor do produto e aplique
+o percentual de acréscimo. O novo valor a ser calculado
+deve ser arredondado para mais ou para menos usando o método
+round. A classe deve também conter um laço de repetição
+que encerre o programa quando o usuário fornecer o valor zero
+(0) para o valor do produto. Dessa forma, o usuário digita o valor do produto, a classe calcula e
+mostra o valor com acréscimo, a seguir solicita um novo valor. Esse processo continua enquanto
+o valor do produto for diferente de zero; caso contrário o programa será encerrado.
+ */
+public class Ex02 {
+    
+}
