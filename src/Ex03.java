14493a5e6be0dca102bf0fd64f8358ef45733051
ValterMonteiro <valtinms@gmail.com>
2022-08-11 20:42:21 -0400

    new commit

---
+++ src/Ex03.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ * 
+ * Crie uma classe que gera um número aleatoriamente (entre 5 e 10)
+por Math.random. Em seguida, faça com que apareça em tela uma
+senha numérica contendo a mesma quantidade de dígitos correspondentes
+ao valor aleatório gerado. Apresente em tela o número
+sorteado e a senha.
+ */
+public class Ex03 {
+    
+}
